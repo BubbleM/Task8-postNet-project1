f2a659514f7e7a1b253f12c62f411114037e4d5e
BubbleM <1870408473@qq.com>
2017-08-18 23:14:01 +0800

    Convert 9-digit postcode to barcode

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -10,11 +10,17 @@ var postcode2barcode = require("../lib/main.js");
 
 describe("Postnet", function(){
 
-    it("should convert postcode to barcode", function(){
+    it("should convert 5-digit postcode to barcode", function(){
         var postcode = '95713';
         var barcode = postcode2barcode(postcode);
         
         expect(barcode).to.equal('||:|:::|:|:|:::|:::||::||::|:|:|');
     });
 
+    it("should convert 9-digit postcode to barcode", function(){
+        var postcode = '123456789';
+        var barcode = postcode2barcode(postcode);
+
+        expect(barcode).to.equal('|:::||::|:|::||::|::|:|:|::||::|:::||::|:|:|:::|:|:|');
+    });
 });
